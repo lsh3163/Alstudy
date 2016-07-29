ac528117e5b4c4e5e9b28f9c44ae540585245171
lsh3163 <easter3163@naver.com>
2016-07-29 20:45:48 +0900

    Create 1654.cpp

---
+++ 1654.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int N;
+long long M;
+vector<long long> group;
+bool decision(long long k){
+	int len = group.size();
+	long long check = 0;
+	for (int i = 0; i < len; i++){
+		check += (group[i] / k);
+	}
+	return check >= M;
+}
+long long optimize(){
+	//0부터 longlong에 있는 모든 수를 우선 정답후보로 봄
+	long long low = 0;
+	long long high = LLONG_MAX;
+	long long mid;
+	//100번만 돌려도 무한루프 안돌고 2의 100승만큼 탐색가능
+	for (int i = 0; i < 100; i++){
+		mid = (low + high) / 2;
+		if (decision(mid)){
+			low = mid;
+		}
+		else{
+			high = mid;
+		}
+	}
+	return low;
+}
+int main(void){
+	cin >> N; cin >> M;
+	group.resize(N);
+	for (int i = 0; i < N; i++){
+		cin >> group[i];
+	}
+	printf("%lld", optimize());
+}
+
